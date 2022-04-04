749d9efde45f0052f45d8fa3f45003bc9b9e1729
rezwanrahimr <rezwanrahim69@gmail.com>
2022-04-04 13:31:37 +0600

    review section complite

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import useReviews from '../../Hook/useReviews';
+import Reviews from '../Reviews/Reviews';
 
 const Home = () => {
+    const [review,setReview] = useReviews();
     return (
-        <div className='container'>
+        <div className='container mb-5'>
            <div className='row'>
                <div className='col-md-6 d-flex align-items-center'>
                    <div>
@@ -21,6 +25,22 @@ const Home = () => {
                <div className='col-md-6'>
                    <img src="https://static-01.daraz.com.bd/p/18b4aa9268a6a1609e370de6f6d0e18b.jpg" alt="" />
                </div>
+           </div>
+           {/* review section */}
+           <div className='container'>
+             <h2>Customar Review</h2>
+             {
+             review.slice(0,3).map(reviews => <div className='review-items ' key={reviews.id}>
+              <div className='review-item my-5'>
+                  <img src={reviews.picture} alt="" />
+                  <h4>{reviews.name}</h4>
+                  <h4>Rating: {reviews.rating} star</h4>
+                  <p>{reviews.text}</p>
+              </div>
+              
+             </div>)
+         }
+         <button className='p-2 rounded px-3 py-1 mt-2 bg-primary border-0 '> <Link className='text-decoration-none text-white' to="/reviews">check reviews</Link></button>
            </div>
         </div>
     );
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import useReviews from '../../Hook/useReviews';
+import './Reviews.css';
+
 
 const Reviews = () => {
+ const [review,setReview] = useReviews();
     return (
         <div>
-            <h1>this is review</h1>
+           
+         {
+             review.map(reviews => <div className='review-items ' key={reviews.id}>
+              <div className='review-item my-5'>
+                  <img src={reviews.picture} alt="" />
+                  <h4>{reviews.name}</h4>
+                  <h4>Rating: {reviews.rating} star</h4>
+                  <p>{reviews.text}</p>
+              </div>
+             </div>)
+         }
         </div>
     );
 };
