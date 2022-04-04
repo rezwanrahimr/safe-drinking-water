7ac25fe0b977ce525403c588b8c94a288d60ef86
rezwanrahimr <rezwanrahim69@gmail.com>
2022-04-04 15:19:00 +0600

    chart added and complite

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [chartdata,setChartdata] = useState([]);
@@ -10,7 +10,7 @@ const Dashboard = () => {
     },[])
     return (
         <div className='container mt-5 row'>
-            <div className='col-md-6'>
+            <div className='col-md-6 p-5'>
             <AreaChart width={530} height={250} data={chartdata}
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <defs>
@@ -30,11 +30,21 @@ const Dashboard = () => {
             <Area type="monotone" dataKey="sell" stroke="#8884d8" fillOpacity={1} fill="url(#colorsell)" />
             <Area type="monotone" dataKey="investment" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
             </AreaChart>
-
-
+ 
+                <div className='mt-5 p-5'>
+                <BarChart width={530} height={250} data={chartdata}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="sell" fill="#8884d8" />
+                <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+                </div>
             </div>
             {/* another */}
-            <div className='col-md-6'>
+            <div className='col-md-6 p-5'>
             <LineChart width={530} height={250} data={chartdata}
             margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
             <CartesianGrid strokeDasharray="3 3" />
@@ -45,6 +55,19 @@ const Dashboard = () => {
             <Line type="monotone" dataKey="sell" stroke="#8884d8" />
             <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
             </LineChart>
+
+            <div className='mt-5 p-5'>
+            <ComposedChart width={530} height={250} data={chartdata}>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid stroke="#f5f5f5" />
+            <Area type="monotone" dataKey="100000" fill="#8884d8" stroke="#8884d8" />
+            <Bar dataKey="revenue" barSize={20} fill="#413ea0" />
+            <Line type="monotone" dataKey="sell" stroke="#ff7300" />
+            </ComposedChart>
+            </div>
             </div>
         </div>
     );
